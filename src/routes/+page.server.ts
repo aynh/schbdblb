43ecffebd3355f311e5660585239c921eb8b5a3d
Ahmad Yasser <99479536+aynh@users.noreply.github.com>
2023-03-12 11:20:12 +0800

    chore: tests

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,17 +1,6 @@
-import { jadwal } from '$lib/server/schedule';
-import { Hari } from '$lib/types';
+import { jadwal, processJadwal } from '$lib/server/schedule';
 import type { PageServerLoad } from './$types';
 
 export const load = (() => {
-	const padded = (n: number) => n.toString().padStart(2, '0');
-	return Object.fromEntries(
-		Object.entries(jadwal).map(([hari, jadwal]) => [
-			Hari[Number(hari)],
-			jadwal.map(({ periode: { mulai, selesai }, ...rest }) => ({
-				...rest,
-				mulai: `${padded(mulai.jam)}:${padded(mulai.menit ?? 0)}`,
-				selesai: `${padded(selesai.jam)}:${padded(selesai.menit ?? 0)}`,
-			})),
-		]),
-	);
+	return processJadwal(jadwal);
 }) satisfies PageServerLoad;
--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import { defineConfig } from 'vitest/config';
 import UnoCSS from 'unocss/vite';
 
 export default defineConfig({
+	define: {
+		'import.meta.vitest': 'undefined',
+	},
 	plugins: [UnoCSS({ mode: 'svelte-scoped' }), sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
+		includeSource: ['src/**/*.{js,ts}'],
 	},
 });
