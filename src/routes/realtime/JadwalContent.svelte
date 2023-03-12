d18113fee2a032ac88d5e7320518498477eb74e5
Ahmad Yasser <99479536+aynh@users.noreply.github.com>
2023-03-12 18:29:54 +0800

    fix: date-fns/locale/id gak bisa dipakai

--- src/routes/realtime/JadwalContent.svelte
+++ src/routes/realtime/JadwalContent.svelte
@@ -2,7 +2,6 @@
 	import { time } from '$lib/stores';
 	import { Hari, type Jadwal } from '$lib/types';
 	import { formatDuration, intervalToDuration, set as setTime } from 'date-fns';
-	import { id } from 'date-fns/locale';
 
 	export let active: boolean;
 	export let hari: Hari;
@@ -14,10 +13,15 @@
 		return setTime($time, { date, hours: jam, minutes: menit, milliseconds: 0, seconds: 0 });
 	});
 
+	const localeReplacement = {
+		day: 'hari',
+		hour: 'jam',
+		minute: 'menit',
+		second: 'detik',
+	} as Record<string, string>;
 	$: duration = formatDuration(
 		intervalToDuration({ start: $time, end: active ? selesai : mulai }),
-		{ locale: id },
-	);
+	).replace(/(day|hour|minute|second)s?/g, (v) => localeReplacement[v.slice(0, -1)]);
 	$: content = active ? `Tinggal ${duration} lagi` : `${duration} lagi mulai`;
 </script>
 
